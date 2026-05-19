bf065ca9a3c1f3363e9925cd88c2a6ce6c199288
Krishnakant Mane <krishnamane712c@gmail.com>
2026-05-19 11:35:22 +0530

    All Files

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -117,7 +117,7 @@ const Dashboard = () => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": "Bearer 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"
+        "Authorization": `Bearer ${import.meta.env.VITE_GROQ_API_KEY}`
       },
       body: JSON.stringify({
         model: "llama-3.3-70b-versatile",
--- frontend/src/pages/ChatAI.jsx
+++ frontend/src/pages/ChatAI.jsx
@@ -74,7 +74,7 @@ const ChatAI = () => {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "Authorization": `Bearer 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`
+          "Authorization": `Bearer ${import.meta.env.VITE_GROQ_API_KEY}`
         },
         body: JSON.stringify({
           model: "llama-3.3-70b-versatile",
